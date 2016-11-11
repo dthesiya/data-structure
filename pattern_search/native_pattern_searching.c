135a53e626ed7eb3db23bbeceb231c4fffbc2247
Priyank Vora <priyank@Priyank.local>
2016-11-11 00:12:03 -0800

    native pattern search

---
+++ pattern_search/native_pattern_searching.c
@@ -0,0 +1,33 @@
+//
+// Created by Priyank Vora on 11/10/16.
+//
+
+#include <stdio.h>
+#include <string.h>
+
+void search_pattern(char *text, char *pat)
+{
+  int M = strlen(text);
+  int N = strlen(pat);
+
+  for (int i=0; i<=M-N; i++)
+  {
+    int j;
+    for (j=0; j<N; j++)
+    {
+      if (text[i+j] != pat[j])
+        break;
+    }
+    if (j == N)
+      printf("pattern at index : %d \n", i);
+  }
+}
+
+
+int main()
+{
+  char text[] = "AABCAABCAADETAABC";
+  char pat[] = "AABC";
+  search_pattern(text, pat);
+  return 0;
+}
